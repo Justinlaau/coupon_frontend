2d314335dcc0e3594ca612ec43e3e409bd5ea00c
Justin-YimYim <55522706+Justin-YimYim@users.noreply.github.com>
2024-01-20 06:17:48 -0800

    merge

--- src/config/config.ts
+++ src/config/config.ts
@@ -3,4 +3,6 @@
 // export const BASE_URL = "http://192.168.31.249:8000/"
 export const BASE_URL = "http://47.129.1.22/"
 
-export const BASE_S3_IMG_URL = "https://coupongo-buckets.s3.us-east-1.amazonaws.com/"
+const BASE_S3_IMG_URL = "https://coupongo-buckets.s3.us-east-1.amazonaws.com/"
+
+export default BASE_S3_IMG_URL;
